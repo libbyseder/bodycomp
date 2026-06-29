2baec8a2cc83287c8827e43870a010b40fb3e451
libbyseder <libby.seder@gmail.com>
2026-06-29 15:10:58 -0400

    Update callback.ts

--- pages/api/withings/callback.ts
+++ pages/api/withings/callback.ts
@@ -2,14 +2,12 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'HEAD') {
-    res.status(200).end()
-    return
+    return res.status(200).end()
   }
 
   if (req.method === 'GET' || req.method === 'POST') {
-    res.status(200).json({ status: 'ok' })
-    return
+    return res.status(200).json({ status: 'ok' })
   }
 
-  res.status(405).end()
+  return res.status(405).end()
 }
